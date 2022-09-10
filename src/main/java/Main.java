09b6ee14c2fa4007b5ebb37d736ca83cc4732b80
Robert Mycek <robert.mycek@gmail.com>
2022-09-10 20:54:37 +0200

    coffeecorner: print receipt for many small coffee

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,38 @@
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
+    private final List<String> products;
+
+    public Main(String input) {
+        this.products = Arrays.stream(Objects.requireNonNull(input).split(",")).collect(toList());
+    }
+
     public static void main(String[] args) throws IOException {
         String input = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
-        System.out.println("Charlene's Coffee Corner");
-        System.out.println("-".repeat(46));
-        System.out.printf((Locale) null, "%-39.39s %6.2f%n", input, 2.5);
-        System.out.println("-".repeat(46));
-        System.out.printf((Locale) null, "%-35.35s %10.2f", "Total CHF", 2.5);
+        String receipt = new Main(input).generateReceipt();
+        System.out.print(receipt);
         System.out.flush();
+    }
+
+    public String generateReceipt() {
+        StringBuffer buffer = new StringBuffer();
+
+        buffer.append("Charlene's Coffee Corner").append('\n');
+        buffer.append("-".repeat(46)).append('\n');
+
+        products.forEach(smallCoffee ->
+                buffer.append(String.format((Locale) null, "%-39.39s %6.2f", smallCoffee, 2.5)).append('\n'));
+
+        buffer.append("-".repeat(46)).append('\n');
+        buffer.append(String.format((Locale) null, "%-35.35s %10.2f", "Total CHF", 2.5 * products.size()));
+
+        return buffer.toString();
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,9 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
-import java.util.List;
 
-import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
@@ -38,14 +36,31 @@ class MainTest {
                 ----------------------------------------------
                 Total CHF                                 2.50""";
 
-        assertEquals(expected.lines().collect(toList()), getOutputLines());
+        assertEquals(expected, getOutput());
+    }
+
+    @Test
+    void shouldPrintReceiptForManySmallCoffee() throws IOException {
+        setInput("small coffee,small coffee");
+
+        Main.main(new String[]{});
+
+        String expected = """
+                Charlene's Coffee Corner
+                ----------------------------------------------
+                small coffee                              2.50
+                small coffee                              2.50
+                ----------------------------------------------
+                Total CHF                                 5.00""";
+
+        assertEquals(expected, getOutput());
     }
 
     private void setInput(String value) {
         System.setIn(new ByteArrayInputStream(value.getBytes()));
     }
 
-    private List<String> getOutputLines() {
-        return output.toString().lines().collect(toList());
+    private String getOutput() {
+        return output.toString().replace("\r", "");
     }
 }
